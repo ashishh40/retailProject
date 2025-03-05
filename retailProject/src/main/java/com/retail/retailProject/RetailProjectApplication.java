3ead5e6be6aa3e24f7d9ee5d3d8fcd296b50e4cf
Ashish Sharma <ashish.sharma@mrisoftware.com>
2025-03-05 15:25:59 +0530

    spring boot empty project with the dependency

---
+++ retailProject/src/main/java/com/retail/retailProject/RetailProjectApplication.java
@@ -0,0 +1,13 @@
+package com.retail.retailProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailProjectApplication.class, args);
+	}
+
+}
